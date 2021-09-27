63b74f3d0b83c5813aa15c270b9c7a9d8c98e5ce
xniola <stefano.perniola0@gmail.com>
2021-09-27 17:42:11 +0200

    Aggiunto foreachbatch

--- src/analysis.py
+++ src/analysis.py
@@ -5,14 +5,23 @@
 
 from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
-from pyspark.sql.types import IntegerType, StringType, StructField, StructType, TimestampType
-
 from pyspark.sql.functions import col
+
+import pandas as pd
+
+
 
 
 sc = SparkContext('local')
 spark = SparkSession(sc)
 #spark.sparkContext.setLogLevel("ERROR")
+
+'''Mappatura dei varchi autostradali con i rispettivi km. Utili per calcolare la distanza chilometrica fra 2 varchi collegati'''
+#tratto di strada orizzontale da sinistra verso destra (relativo alla mappa)
+or_sx_dx = pd.DataFrame({'varco': [27,9,26,10,18,23,15,5,8,3,13], 'km': [22.6, 31.08,48.5,54.5,66.8,80.8,98.4,106.1,117.8,124.7,134.5]})
+
+#tratto di strada orizzontale da destra verso sinistra (relativo alla mappa)
+or_dx_sx = pd.DataFrame({'varco': [22,1,12,25,20,2,16,4,21], 'km': [135.3,120,113.8,106.1,88.4,74.6,60.5,46.5,20.8]})
 
 
 # Mi iscrivo al topic di kafka
@@ -35,32 +44,14 @@ df = dati \
                  ,"split(value,',')[4] as nazione"
                  )
 
+def foreach_batch_function(df, epoch_id):
+    df.select("targa").show()
 
 # Query del dataframe appena inferito
-
 query = df     \
   .writeStream  \
   .format("console") \
   .outputMode("append") \
+  .foreachBatch(foreach_batch_function)\
   .start()
-
-print(type(query))
 query.awaitTermination()
-
-'''
-query2 = df.select('varco').groupby().sum()\
-  .writeStream\
-  .format("console")\
-  .outputMode("append")\
-  .start()\
-  .awaitTermination()
-'''
-'''
-query1 = df.collect.foreach(print)\
-  .writeStream\
-  .format("console")\
-  .start()
-
-query1.awaitTermination()
-'''
-
