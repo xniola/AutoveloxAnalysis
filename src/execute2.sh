f2ce9d9885c8fd1984b1f49ed4dade0ea880d791
xniola <stefano.perniola0@gmail.com>
2021-10-07 22:58:48 +0200

    Ricezione da kafka del secondo worker

---
+++ src/execute2.sh
@@ -0,0 +1,1 @@
+spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 num_cars2.py
--- src/num_cars.py
+++ src/num_cars.py
@@ -1,17 +1,17 @@
-
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StringType, StructType, StructField
-from pyspark.sql.types import IntegerType
-from pyspark.sql.functions import concat, split, col, from_csv, struct, to_json
-from pyspark.sql.functions import lit
+from pyspark.sql.types import DoubleType, StructType, StructField
+from pyspark.sql.types import StringType, IntegerType, ArrayType, TimestampType
+from pyspark.sql.functions import concat, count, split, col, from_json, from_csv, udf, expr
+from pyspark.sql.functions import lit, window, unix_timestamp, count, max, first, struct
 
 spark = SparkSession \
     .builder \
-    .appName("CarCount") \
+    .appName("LastSeen") \
     .getOrCreate()
 
 
 # Stream
+
 dfstream = spark \
     .readStream \
     .format("kafka") \
@@ -19,16 +19,14 @@ dfstream = spark \
     .option("subscribe", "autovelox") \
     .load()
 
+
+options = {'sep': ','}
+schema = "targa INT, varco INT, corsia DOUBLE, timestamp TIMESTAMP, nazione STRING"
+
 dfstream = dfstream.selectExpr("CAST(value AS STRING)") \
     .select(from_csv(col("value"), schema, options)
             .alias("data")) \
     .select("data.*")
-
-in_stream = dfstream \
-    .select(col("targa"), col("varco"), col("corsia"), col("timestamp"), col("nazione")) \
-    .withColumnRenamed("targa", "in_targa") \
-    .withColumnRenamed("timestamp", "in_timestamp") \
-    .withWatermark("in_timestamp", "3 minutes")
 
 
 # Immette i tratti autostradali
@@ -36,11 +34,11 @@ tratti = [(1, 27, 9), (2, 9, 26), (3, 26, 10), (4, 10, 18), (5, 18, 23),
           (6, 23, 15), (7, 15, 5), (8, 5, 8), (9, 8, 3), (10, 3, 13),
           (11, 22, 1), (12, 1, 12), (13, 12, 25), (14, 25, 20), (15, 20, 2),
           (16, 2, 16), (17, 16, 4), (18, 4, 21)]
+
 tratti_schema = StructType([
     StructField('tratto', IntegerType()),
-    StructField('entrata', IntegerType()),
-    StructField('uscita', IntegerType())
-])
+    StructField('ingresso', IntegerType()),
+    StructField('uscita', IntegerType())])
 
 df_tratti = spark.createDataFrame(data=tratti, schema=tratti_schema).cache()
 
@@ -49,34 +47,34 @@ df_tratti = spark.createDataFrame(data=tratti, schema=tratti_schema).cache()
 #                       si trova
 
 ultimi_avvistamenti = dfstream.join(
-    df_tratti, dfstream.varco == df_tratti.entrata, 'inner') \
+    df_tratti, dfstream.varco == df_tratti.ingresso, 'inner') \
     .groupBy("targa") \
-    .agg(max("timestamp"), count(lit(1)), first("tratto")) \
-    .orderBy('count(1)', ascending=False)
-# .withWatermark("timestamp", "3 minutes") \
-# .orderBy('timestamp', ascending=False)
-conteggio = ultimi_avvistamenti.groupBy("tratto").count()
-# stream-stream join
+    .agg(
+        max(struct("timestamp", "tratto", "ingresso")).alias("max"),
+        count(lit(1)).alias("avvistamenti"),
+        first("nazione").alias("nazione")) \
+    .select("targa", "max.timestamp", "max.tratto", "max.ingresso", "avvistamenti", "nazione") \
+    .orderBy('timestamp', ascending=False)
 
 
-sink = conteggio.select(concat("tratto", lit(" "), "sum(count)", lit(" ")).alias("value")) \
-.writeStream \
-.format("kafka")\
-.outputMode("update") \
-.option("kafka.bootstrap.servers", "localhost:9092")\
-.option("topic", "risultati")\
-.option("checkpointLocation", "kafka_sink")\
-.start()
 
-sink.awaitTermination()
+# Output
 
 
-'''
-query = sink = conteggio.select(concat("tratto", lit(" "), "sum(count)", lit(" ")).alias("value")) \
-.writeStream \
-.format("console")\
-.outputMode("update") \
-.start()
+query = ultimi_avvistamenti.select(concat(
+    "targa", lit(" "),
+    "timestamp", lit(" "),
+    "tratto", lit(" "),
+    "ingresso", lit(" "),
+    "avvistamenti", lit(" "),
+    "nazione"
+    ).alias("value")) \
+    .writeStream\
+    .format("kafka")\
+    .outputMode("complete")\
+    .option("kafka.bootstrap.servers", "localhost:9092")\
+    .option("checkpointLocation", "kafka_sink")\
+    .option("topic", "ultimi-avvistamenti")\
+    .start() 
 
 query.awaitTermination()
-'''
